2e8cef8b9deb39d8e7889f0ea1c229cf371b5f2b
le2sky <leehaneul990623@gmail.com>
2023-11-13 00:41:45 +0900

    style : 불필요한 공백 제거

--- src/main/java/christmas/presentation/exception/ExceptionHandler.java
+++ src/main/java/christmas/presentation/exception/ExceptionHandler.java
@@ -9,7 +9,6 @@ public class ExceptionHandler {
     private static final String UNEXPECTED_MESSAGE = "예기치 못한 에러가 발생했습니다.";
 
     private ExceptionHandler() {
-
     }
 
     public static <T> T handle(Supplier<T> supplier, String message) {
